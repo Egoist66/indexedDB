bdf0c9b19ca27af5adcf66b963e1f6213fbe500c
Farid Mahmudov <Razormad666@gmail.com>
2024-10-12 14:04:22 +0300

    update

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,4 +1,4 @@
-import {openDB} from './indexedDB+.js';
+import {openDB} from '../lib/indexedDB+.js';
 
 
 /**
@@ -8,13 +8,14 @@ import {openDB} from './indexedDB+.js';
  */
 export async function connect(){
 
-    await openDB('users', 1, {
+    const usersDB =  await openDB('users', 1, {
         upgrade(db) {
             if (!db.objectStoreNames.contains('users-storage')) {
                 const store = db.createObjectStore('users-storage', {
                     keyPath: 'email',
+                    unique: true
                 })
-                console.log(store)
+                
             }
             else {
                 console.log('Database already exists');
@@ -23,9 +24,34 @@ export async function connect(){
         },
     });
 
+    const tx = usersDB.transaction('users-storage', 'readwrite');
+ 
+
+    const res = await Promise.all([
+        tx.store.add({
+            name: 'Admin',
+            email: 'admin@example.ru',
+            password: 'admin',
+            role: 'admin'
+        }),
+        tx.store.add({
+            name: 'User',
+            email: 'user@example.ru',
+            password: 'user',
+            role: 'user'
+        }),
+        tx.done
+    ])
+
+
+    const data = await usersDB.get('users-storage', 'admin@example.ru')
+    console.log(data)
+    
+
+  
 
     
-    await openDB('notes', 2, {
+    const notesDB = await openDB('notes', 2, {
         upgrade(db) {
             if (!db.objectStoreNames.contains('notes-storage')) {
                 const store = db.createObjectStore('notes-storage', {
@@ -45,3 +71,5 @@ export async function connect(){
     });
   
 }
+
+
